05a8a896b273d656ed6161bc33b5e75bc812771f
Dustin Dowell <dustindowell22@gmail.com>
2015-02-08 18:38:19 -0600

    Convert from tabs to 2 spaces

--- jquery.image-snap.js
+++ jquery.image-snap.js
@@ -6,75 +6,75 @@
 
 
 (function( $ ) {
-	$.fn.imageSnap = function( imageSourceAttribute ) {
+  $.fn.imageSnap = function( imageSourceAttribute ) {
 
-		// ====================
-		// Preferences
-		// ====================
+    // ====================
+    // Preferences
+    // ====================
 
-			// Image selector
-			var imageSelector = $( this );
+      // Image selector
+      var imageSelector = $( this );
 
-			// Image source attribute
-			// Note:
-			// + This should be set to 'src' or 'data-src'.
-			// + Setting this to 'data-src' will require you to use 'data-src' on all images that you want to use with Image Snap.
-			// + Setting this to 'src' may load each image an additional time in some browsers. Others may check the cache first.
-			if( imageSourceAttribute === undefined ) {
-				imageSourceAttribute = 'data-src';
-			}
+      // Image source attribute
+      // Note:
+      // + This should be set to 'src' or 'data-src'.
+      // + Setting this to 'data-src' will require you to use 'data-src' on all images that you want to use with Image Snap.
+      // + Setting this to 'src' may load each image an additional time in some browsers. Others may check the cache first.
+      if( imageSourceAttribute === undefined ) {
+        imageSourceAttribute = 'data-src';
+      }
 
-		// ====================
+    // ====================
 
 
-		function imageSnap() {
-			$( imageSelector ).each( function() {
+    function imageSnap() {
+      $( imageSelector ).each( function() {
 
-				// This selector
-				var $image = $( this );
+        // This selector
+        var $image = $( this );
 
-				// Image source
-				var imageSrc = $image.attr( imageSourceAttribute );
+        // Image source
+        var imageSrc = $image.attr( imageSourceAttribute );
 
-				// Variables
-				var parentWidth = parseInt( $image.parent().css( 'width' ) );
-				var lineHeight = parseInt( $image.css( 'line-height' ) );
+        // Variables
+        var parentWidth = parseInt( $image.parent().css( 'width' ) );
+        var lineHeight = parseInt( $image.css( 'line-height' ) );
 
-				// Creates new image in-memory, sets image source and executes on image load
-				$( '<img>' ).attr( 'src', imageSrc ).load( function() {
+        // Creates new image in-memory, sets image source and executes on image load
+        $( '<img>' ).attr( 'src', imageSrc ).load( function() {
 
-					// Native image dimensions
-					// Note: $( this ) won't work on in memory images.
-					var nativeWidth = this.width;
-					var nativeHeight = this.height;
+          // Native image dimensions
+          // Note: $( this ) won't work on in memory images.
+          var nativeWidth = this.width;
+          var nativeHeight = this.height;
 
-					// Logic
-					var imageRatio = nativeWidth / nativeHeight;
-					var newHeight = Math.floor( ( nativeWidth / imageRatio ) / lineHeight ) * lineHeight;
-					var newWidthPercentage = ( newHeight * imageRatio ) / parentWidth;
+          // Logic
+          var imageRatio = nativeWidth / nativeHeight;
+          var newHeight = Math.floor( ( nativeWidth / imageRatio ) / lineHeight ) * lineHeight;
+          var newWidthPercentage = ( newHeight * imageRatio ) / parentWidth;
 
-					// Resize image
-					if( newWidthPercentage > (2/3) ) {
-						$image.css({
-							width: '100%',
-							height: 'auto'
-						});
-					}
-					else {
-						$image.css({
-							width: 'auto',
-							height: newHeight + 'px'
-						});
-					}
-				});
+          // Resize image
+          if( newWidthPercentage > (2/3) ) {
+            $image.css({
+              width: '100%',
+              height: 'auto'
+            });
+          }
+          else {
+            $image.css({
+              width: 'auto',
+              height: newHeight + 'px'
+            });
+          }
+        });
 
-				// Load image
-				$image.attr( 'src', imageSrc );
-			});
-		}
+        // Load image
+        $image.attr( 'src', imageSrc );
+      });
+    }
 
-		// Execute function
-		$( document ).on( 'ready', imageSnap );
-		$( window ).on( 'resize', imageSnap );
-	};
+    // Execute function
+    $( document ).on( 'ready', imageSnap );
+    $( window ).on( 'resize', imageSnap );
+  };
 }( jQuery ));
